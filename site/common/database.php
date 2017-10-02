18beca166ab04dc05a07d7d9f66c5cbc474c3e8e
ChrisSheng97 <chrissheng97@gmail.com>
2017-10-02 13:37:46 -0400

    Update database.php

--- site/common/database.php
+++ site/common/database.php
@@ -752,7 +752,7 @@ Output:
 function resetEpicast(&$output, $year, $firstEpiweek, $lastEpiweek, $deadline, $admin) {
    $tbl_old = ($year - 1) . "_ec_fluv_";
    $tbl_new = 'ec_fluv_';
-   $tables = array('defaults', 'forecast', 'history', 'regions', 'round', 'scores', 'season', 'submissions', 'user_preferences', 'users', 'age_groups');
+   $tables = array('defaults', 'forecast', 'history', 'regions', 'round', 'scores', 'season', 'submissions', 'user_preferences', 'users', 'age_groups', 'forecast_hosp', 'submissions_hosp');
    foreach($tables as $name) {
       mysql_query("CREATE TABLE {$tbl_old}{$name} AS SELECT * FROM {$tbl_new}{$name}");
    }
