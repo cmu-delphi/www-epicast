69105d8d61181ebe9d86c3871f8174f99d697f39
ChrisSheng97 <chrissheng97@gmail.com>
2017-10-12 08:52:10 -0400

    Update database.php

--- site/common/database.php
+++ site/common/database.php
@@ -300,6 +300,47 @@ function getHistory(&$output, $regionID, $firstWeek) {
 }
 
 /*
+===== getHistory_Hosp =====
+Purpose:
+   Returns history for an ageGroup
+Input:
+   $output - The array of return values (array reference)
+   $ageGroup - The age group's ID
+   $firstWeek - The first epiweek
+Output:
+   $output['result'] will contain the following values:
+      1 - Success
+      2 - Failure
+   $output['history'] - Arrays of epiweeks and historical incidence (wILI) for the region
+*/
+function getHistory_Hosp(&$output, $ageGroup, $firstWeek) {
+   $result = mysql_query(SELECT `flusurv`.`issue`, 
+                         `flusurv`.`epiweek`, 
+                         `flusurv`.`rate_age_3` AS `rate` FROM 
+                         (SELECT `epiweek`, max(`issue`) AS `latest` FROM `flusurv` WHERE 
+                          `location` = 'network_all' AND `epiweek` >= 201710 GROUP BY `epiweek`) 
+                         AS `issues` JOIN `flusurv` ON `flusurv`.`issue` = `issues`.`latest` 
+                         AND `flusurv`.`epiweek` = `issues`.`epiweek` WHERE `location` = 'network_all' 
+                         ORDER BY `flusurv`.`epiweek` ASC;);
+   $date = array();
+   $rate = array();
+   while($row = mysql_fetch_array($result)) {
+      $ew = intval($row['epiweek']);
+      while($firstWeek < $ew) {
+        array_push($date, $firstWeek);
+        array_push($wili, -1);
+        $firstWeek = addEpiweeks($firstWeek, 1);
+      }
+      array_push($date, $ew);
+      array_push($rate, floatval($row['rate']));
+      $firstWeek = addEpiweeks($firstWeek, 1);
+   }
+   $output['history'] = array('date' => &$date, 'rate' => &$rate);
+   setResult($output, 1);
+   return getResult($output);
+}
+
+/*
 ===== saveForecast =====
 Purpose:
    Saves the user's forecast
