868792b2333a1675e61487a851fa7bfce3091f3e
Chris Sheng <chrissheng97@gmail.com>
2017-10-22 18:02:52 -0700

    Move some constants to a separate js file

--- site/forecast.php
+++ site/forecast.php
@@ -180,26 +180,10 @@ foreach($output['regions'] as $r) {
          ?>
       </div></div><div id="box_canvas"><canvas id="canvas" width="800" height="400"></canvas></div>
 </div>
+<script src="js/forecast.js"></script> 
 <script>
    //globals
    //var DEBUG = <?= $output['user_id'] == 9 ? 'true' : 'false' ?>;
-   var DRAW_POINTS = true;
-   var TICK_SIZE = 5;
-   var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-   var LABEL_X = 'Date (Epiweek, Month)';
-   var LABEL_Y = 'Flu Activity (wILI)';
-   var MARGIN_LEFT = 60;
-   var MARGIN_BOTTOM = 60;
-   var MARGIN_RIGHT = 12;
-   var MARGIN_TOP = 12;
-   var DASH_STYLE = [1, 5];
-   var AUTOSAVE_INTERVAL = 1;
-   var AXIS_STYLE = {color: '#000', size: 1, dash: []};
-   var GRID_STYLE = {color: '#bbb', size: 1, dash: DASH_STYLE};
-   var ZOOM_AMOUNT = 1.1;
-   var BUTTON_SIZE = 25;
-   var WILI_MAX = 20;
-   var WILI_MIN = 3;
    //Axis range
    var currentWeek = <?= $currentWeek ?>;
    var numPastWeeks = <?= $numPastWeeks ?>;
@@ -207,28 +191,7 @@ foreach($output['regions'] as $r) {
    var totalWeeks = (numPastWeeks + 1 + numFutureWeeks);
    var xRange = [addEpiweeks(currentWeek, -numPastWeeks), addEpiweeks(currentWeek, +numFutureWeeks)];
    var yRange = [0, <?= ($maxRegionalWILI * 1.1) ?>];
-   //Number of axis tick marks
-   var xInterval = 2;
-   var yInterval = 1;
-   var uiScale = 1;
-   var canvas = $('#canvas')[0];
-   var dragging = false;
-   var hoveringButton = null;
    var regionID = <?= $regionID ?>;
-   var modifyCounter = 0;
-   var submitCounter = 0;
-   var modified = false;
-   var zoomDownBounds;
-   var zoomUpBounds;
-   var showLastBounds;
-   var snapLastBounds;
-   var SubmitStatus = {
-      init: 0,
-      sent: 1,
-      success: 2,
-      failure: 3
-   };
-   var submitStatus = SubmitStatus.init;
    var seasonOffsets = [<?php foreach($seasonOffsets as $o){printf('%d,',$o);} ?>];
    var seasonYears = [<?php foreach($seasonYears as $y){printf('%d,',$y);} ?>];
    var seasonIndices = {<?php for($i = 0; $i < count($seasonYears); $i++){printf('\'%d\':%d,',$seasonYears[$i],$i);} ?>};
@@ -278,11 +241,6 @@ foreach($output['regions'] as $r) {
       printf('var nowcast = [%.5f, %.5f];', $output['nowcast']['value'], $output['nowcast']['std']);
    }
    ?>
-   //chart bounds
-   function marginLeft() { return MARGIN_LEFT * uiScale; }
-   function marginRight() { return MARGIN_RIGHT * uiScale; }
-   function marginTop() { return MARGIN_TOP * uiScale; }
-   function marginBottom() { return MARGIN_BOTTOM * uiScale; }
    //x-axis
    function getChartWidth() {
       return canvas.width - marginLeft() - marginRight();
