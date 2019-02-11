37b38410ec8548dfbda30798b2da359bdc83ec9a
ChrisSheng97 <chrissheng97@gmail.com>
2019-02-11 13:07:20 -0500

    Add files via upload

--- site/api_mturk.php
+++ site/api_mturk.php
@@ -7,6 +7,7 @@ require_once('common/database.php');
 //Connect to the database
 $dbh = databaseConnect($dbHost, $dbPort, $dbUser, $dbPass, $dbName);
 
+
 //Create the output array
 $output = array('result' => 0);
 if(!$dbh) {
@@ -16,21 +17,19 @@ if(!$dbh) {
    //Connected successfully
    if($_REQUEST['action'] == 'forecast' || $_REQUEST['action'] == 'autosave') {
       $output['action'] = $_REQUEST['action'];
-      $mturkID = mysqli_real_escape_string($_REQUEST['mturkID']);
-      $hash = mysqli_real_escape_string($_REQUEST['hash']);
+      $mturkID = mysqli_real_escape_string($dbh, $_REQUEST['mturkID']);
+      $hash = mysqli_real_escape_string($dbh, $_REQUEST['hash']);
       $id = $_REQUEST['userID'];
-      // echo ("saving forecast for: ");
-      // echo ($mturkID);
       $temp = array();
       if(userAlreadyExist($mturkID) == 1) {
          $forecast = array();
          foreach($_REQUEST['f'] as $f) {
-            array_push($forecast, floatval(mysqli_real_escape_string($f)));
+            array_push($forecast, floatval(mysqli_real_escape_string($dbh, $f)));
          }
          if(getEpiweekInfo($temp) == 1) {
             if(count($forecast) >= 1 && count($forecast) <= 53) {
                //Save the forecast
-               $regionID = intval(mysqli_real_escape_string($_REQUEST['region_id']));
+               $regionID = intval(mysqli_real_escape_string($dbh, $_REQUEST['region_id']));
                $commit = ($_REQUEST['action'] == 'forecast');
                // $id = getUserIDByMturkID($mturkID);
                if(saveForecast_mturk($temp, $id, $regionID, $forecast, $commit) == 1) {
