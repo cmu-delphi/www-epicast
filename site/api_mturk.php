359b45e9432c595c7665db8d4d2ffdd7e6594ba9
ChrisSheng97 <jiaxians@andrew.cmu.edu>
2019-02-24 13:22:49 -0500

    Update api_mturk.php

--- site/api_mturk.php
+++ site/api_mturk.php
@@ -17,10 +17,7 @@ if(!$dbh) {
    //Connected successfully
    if($_REQUEST['action'] == 'forecast' || $_REQUEST['action'] == 'autosave') {
       $output['action'] = $_REQUEST['action'];
-//       $mturkID = mysql_real_escape_string($_REQUEST['mturkID']);
-//       $hash = mysql_real_escape_string($_REQUEST['hash']);
-      $mturkID = $_REQUEST['mturkID'];
-      $hash = $_REQUEST['hash'];
+      $mturkID = mysql_real_escape_string($_REQUEST['mturkID']);
       $id = $_REQUEST['userID'];
       $temp = array();
       if(userAlreadyExist($mturkID) == 1) {
