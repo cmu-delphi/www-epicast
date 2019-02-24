ba13d90d0ab52dc703a03634aae33f59753b3dc3
ChrisSheng97 <chrissheng97@gmail.com>
2019-02-24 17:14:29 -0500

    snapshot - hardcode initiating regionID

--- site/forecast_mturk.php
+++ site/forecast_mturk.php
@@ -21,7 +21,6 @@ function getColor($regionID, $seasonID) {
 
 $regionID = intval($_GET["id"]);
 $mturkID = $_GET['mturkId'];
-$isFirsRegion = $_GET['isFirsRegion'];
 $userID = getUserIDByMturkID($mturkID);
 
 loadUserPreferences_mturk($output, $userID);
@@ -103,7 +102,7 @@ $seasonOffsets = array_reverse($seasonOffsets);
 $seasonYears = array_reverse($seasonYears);
 //$seasonYears = array_slice($seasonYears, 0, count($seasonYears));
 
-if($isFirsRegion && getPreference($output, 'skip_instructions', 'int') !== 1) {
+if(($regionID=== 17 || $regionID=== 12 || $regionID=== 21 || $regionID=== 14 || $regionID=== 25) && getPreference($output, 'skip_instructions', 'int') !== 1) {
    ?>
    <div class="box_section">
       <div class="box_section_title">
