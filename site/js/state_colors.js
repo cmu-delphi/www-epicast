9ce5b4054bba1d59c64c885524afcf46ea18b173
ChrisSheng97 <chrissheng97@gmail.com>
2019-10-10 22:41:15 -0400

    Update state_colors.js

--- site/js/state_colors.js
+++ site/js/state_colors.js
@@ -19,29 +19,27 @@ function getStates(region) {
    
    var style = {};
    
-//    // Case 1: region is a single state.
-//    if (region == 55) { // CA
-//       style['CA'] = {fill: '#9dcbdb'};
-//    } else if (region == 13) { // DC
-//       style['MD'] = {fill: '#b2721a'};
-//    } else if (region == 12) { // GA
-//       style['GA'] = {fill: '#dbd543'};
-//    } else if (region == 0) { // Case 2: region is national.
-//       var num_hhs_regions = 10;
-//       for(var i = 0; i < num_hhs_regions; i++) {
-//          for(var j = 0; j < regionStates[i].length; j++) {
-//             style[regionStates[i][j]] = {fill: regionColors[i]};
-//          }
-//       }
-//    } else { // Case 3: region is a HHS region that contains multiple states.
-//       var states = regionStates[region - 1]; // here, hhs1 corresponds to region 1, which is the 0th index in regionStates.
-//       var region_color = regionColors[region - 1];
-//       for(var i = 0; i < states.length; i++) {
-//          style[states[i]] = {fill: region_color};
-//       }
-//    }
-   
-   style['MD'] = {fill: '#b2721a'};
-   
+   // Case 1: region is a single state.
+   if (region == 55) { // CA
+      style['CA'] = {fill: '#9dcbdb'};
+   } else if (region == 13) { // DC
+      style['MD'] = {fill: '#b2721a'};
+   } else if (region == 12) { // GA
+      style['GA'] = {fill: '#dbd543'};
+   } else if (region == 0) { // Case 2: region is national.
+      var num_hhs_regions = 10;
+      for(var i = 0; i < num_hhs_regions; i++) {
+         for(var j = 0; j < regionStates[i].length; j++) {
+            style[regionStates[i][j]] = {fill: regionColors[i]};
+         }
+      }
+   } else { // Case 3: region is a HHS region that contains multiple states.
+      var states = regionStates[region - 1]; // here, hhs1 corresponds to region 1, which is the 0th index in regionStates.
+      var region_color = regionColors[region - 1];
+      for(var i = 0; i < states.length; i++) {
+         style[states[i]] = {fill: region_color};
+      }
+   }
+      
    return style;
 }
