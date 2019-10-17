cef482b61ef0736c0ebb5e3b79ff9f177f278995
Roberto Iriondo <contact@robertoiriondo.com>
2019-10-17 12:57:35 -0400

    Added Service Worker
    Tested addition of service worker on cloned environment with significant improvement.

---
+++ site/sw.js
@@ -0,0 +1,81 @@
+//Initialize Service Worker Properly
+"use strict";
+var workbox = 0;
+"function" == typeof importScripts &&
+  importScripts (
+    "https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js"
+  );
+
+console.log('Was Workbox able to load?');
+  if (workbox) {
+    console.log('Workbox loaded correctly.');
+  } else {
+    console.log('Workbox did not load correctly, please check your service worker for errors.');
+  }
+
+workbox.googleAnalytics.initialize();
+
+workbox.routing.registerRoute(
+  // Cache JS files
+  /.*\.php/,
+  // Use cache but update in the background ASAP
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'php-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  // Cache CSS files
+  /.*\.css/,
+  // Use cache but update in the background ASAP
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'css-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  // Cache JS files
+  /.*\.js/,
+  // Use cache but update in the background ASAP
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'js-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  // Cache Image files
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  // Use the cache if it's available
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache up to 50 images
+        maxEntries: 50,
+        // Cache for a maximum of 28 days
+        maxAgeSeconds: 28 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  /.*(?:epicast)\.org.*$/,
+  workbox.strategies.staleWhileRevalidate({
+  cacheName: 'internal-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  /.*(?:googleapis|ajax.googleapis|fonts.googleapis|fonts.gstatic|maxcdn.bootstrapcdn)\.com.*$/,
+  workbox.strategies.staleWhileRevalidate({
+  cacheName: 'external-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  /.*(?:delphi.midas.cs.cmu)\.edu.*$/,
+  workbox.strategies.staleWhileRevalidate({
+  cacheName: 'API-cache',
+  })
+);
