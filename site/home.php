0169c570fd4d2a7d4807f35615820fc890aa1a00
ChrisSheng97 <chrissheng97@gmail.com>
2019-10-23 23:28:00 -0400

    Update home.php

--- site/home.php
+++ site/home.php
@@ -56,7 +56,7 @@ if($_offline) {
          <div class="box_section_subtitle">
             Due by 10:00 AM (ET) on <?= date('l, M j, Y', $output['epiweek']['deadline_timestamp']) ?>.
          </div>
-         <div class="box_section_title">
+         <div class="box_section_subtitle">
             The competition is starting this Friday, October 25.  You will get an email when the new data becomes available.
          </div>
 <!--          <div class="box_section_subtitle">
