31fb823cfce6f64b6dac3eba6fdb71fcc045418e
ChrisSheng97 <chrissheng97@gmail.com>
2020-03-13 16:13:42 -0400

    update deadline for first virus round

--- site/home.php
+++ site/home.php
@@ -39,8 +39,12 @@ if($_offline) {
       <div class="box_section_title">
          Post <?= formatEpiweek($output['epiweek']['round_epiweek']) ?> Forecast
          
-         <div class="box_section_subtitle">
+<!--          <div class="box_section_subtitle">
             Due by 10:00 AM (ET) on <?= date('l, M j, Y', $output['epiweek']['deadline_timestamp']) ?>.
+         </div> -->
+         
+         <div class="box_section_subtitle">
+            Due by 10:00 AM (ET) on Thursday ?>.
          </div>
       </div>
       <div>
