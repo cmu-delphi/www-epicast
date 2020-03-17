e3e0998700f98d8408b944bdd5de1a2ea91632f6
ChrisSheng97 <chrissheng97@gmail.com>
2020-03-17 13:49:19 -0400

    add explanation

--- site/home.php
+++ site/home.php
@@ -48,6 +48,24 @@ if($_offline) {
          </div>
       </div>
       <div>
+         
+    <div class="box_section">
+      <div class="box_section_title">
+         <div class="box_section_subtitle">
+            *** The following few paragraphs are new and important – please read them carefully ***
+
+             </br>
+
+         Due to the coronavirus pandemic, and at the request of CDC, we will be continuing our Epicast activity (now renamed ‘Crowdcast’) for as long as needed.  We ask you to continue to forecast exactly the same thing you have been forecasting until now -- the prevalence of influenza-like- illness (ILI).  In the coming months, as flu and other winter respiratory illnesses recede, the majority of ILI is expected to be attributable to COVID-19.
+             </br>
+         One big change to expect is that a pandemic such as COVID-19 tends to be much larger than a typical flu epidemic wave, and can occur at any time of the year.  Another thing to expect is that strong mitigation efforts may dampen the pandemic wave.
+             </br>
+         Starting this week, we will change the Crowdcast interface to provide you with more COVID-related and pandemic-related information and links.
+         </div>
+
+   </div>
+         
+         
          <?php
          $time = $output['epiweek']['remaining'];
          $value = '';
