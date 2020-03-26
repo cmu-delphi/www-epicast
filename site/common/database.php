2951d6d83cb1a6e7197f6b3fc63969ff14e16c47
ChrisSheng97 <chrissheng97@gmail.com>
2020-03-26 16:46:51 -0400

    snapshot - locations based on user ID

--- site/common/database.php
+++ site/common/database.php
@@ -1116,7 +1116,7 @@ function get_user_forecast_regions($user_ID) {
 
     $query = "SELECT states FROM ec_fluv_mturk_tasks WHERE `taskID` = {$userID}";
     $regionIDs = array();
-    $regionIDs = readSqlResult($query, $availableTasks);
+    $regionIDs = readSqlResult($query, $regionIDs);
 
     return $regionIDs;
 }
