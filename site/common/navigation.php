336582da0427aa95468112db1255efdf38e6dac2
ChrisSheng97 <chrissheng97@gmail.com>
2020-03-26 16:26:11 -0400

    snapshot - locations based on user ID

--- site/common/navigation.php
+++ site/common/navigation.php
@@ -9,6 +9,7 @@
 </script>
 
 <?php
+require('common/database.php');
 function showRegionButton($r) {
    if($r['completed']) {
       $class = 'box_region_nav_complete';
@@ -101,11 +102,9 @@ function showNavigation_hosp($output, $getUrl) {
 }
 
 function showNavigation($output, $regionID=-1) {
-   $missing = 0;
-   $submitted = 0;
-   $regionIDs = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 56); // 56: CA, 13, 14: GA, DC
-
-   echo($output['user_id']);
+    $missing = 0;
+    $submitted = 0;
+    $regionIDs = get_user_forecast_regions($output['user_id']);
 
    foreach ($regionIDs as $i) {
         $r = $output['regions'][$i];
