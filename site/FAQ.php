dc1e033a80f4b1eadf65ea6fe23aebd31e1e1b00
Kathryn M Mazaitis <krivard@cs.cmu.edu>
2020-03-28 09:27:03 -0400

    Incorporated Roni's FAQ updates as of 9:30AM 28 Mar

--- site/FAQ.php
+++ site/FAQ.php
@@ -5,52 +5,62 @@ require_once('common/navigation.php');
 <div>
   <center><font size="6">FAQ </font></center>
 
-  <center><p>
-       In this page we collect and answer some frequently asked questions we receive. All of the questions are valid, as
-       well as the alternative suggestions. We are also gearing up to forecast other measures.
-  </p></center>
+  <center><p> In this page we collect and answer some frequently asked
+  questions we receive. All questions, however simple or complicated,
+  are welcome, as are suggestions for improvements.  </p></center>
 
   </br>
-
-  <p><font size="5">
-    1. what data I should be basing a forecast on, is it the situation of people in my current state?
-  </p></font>
+  
+  <p><font size="5"> 1. what data I should be basing a forecast on, is
+    it the situation of people in my current state?
+  </font></p>
 
 
   <p>
-    Answer: TODO
+    Answer: You should forecast whatever you think will actually
+    happen in the location you are forecasting.
   </p>
   </br>
 
 
-  <p><font size="5">
-    2. Is "% of doctor visits that involve flu-like symptoms" the only forecast you are interested in?
-    "This seems like the wrong measure - I think many doctors offices will be shut down for “visits” to
-     free up resources for treating critical Covid-19 patients."
-  </p></font>
+  <p><font size="5"> 2. Why did you choose to forecast "% of doctor
+    visits that involve flu-like symptoms"?  Is this the only measure
+    you are interested in?
+  </font></p>
 
 
   <p>
-    Answer: The CDC and other epidemiologists believe that this is the most important measure to forecast.
-    While forecasting, however, you can factor in your belief that doctors offices will be shut down for
-    routine visits.
+    Answer: This is the measure CDC asked us to forecast first.  An
+    elaborate system for measuring it already exists, and is being
+    re-purposed to measure covid-19 levels.  We will likely also be
+    asked to forecast measures related to covid-19 hospitalization.
   </p>
   </br>
 
 
-  <p><font size="5">
-    3. “I think the best thing to nowcast would be number of true cases, and to forecast would be number of deaths from
-    Covid-19 or perhaps excess mortality, to capture all cause deaths that occur because the hospital system is
-    completely overwhelmed."
-  </p></font>
+  <p><font size="5"> 3. "This seems like the wrong measure - I think
+  many COVID-19 patients may be diagnosed over the phone, and many
+  routine visits may be cancelled during this period.”
+  </font></p>
+
+  <p>
+    Answer: In that case, you should factor these beliefs into your
+    forecasts.
+  </p>
+</br>
+
+  <p><font size="5"> 4. “I think the best thing to nowcast would be
+    the number of true cases; and the best thing to forecast would be
+    the number of COVID-19 deaths, or perhaps excess mortality, to
+    capture deaths from all causes that occur because the healthcare
+    system is overwhelmed."
+  </font></p>
 
 
    <p>
-    Answer: This is a very valid question, and the alternatives suggtested are valid as well. Currently we're forecasting
-    "% of doctor visits that involve flu-like symptoms", which what CDC asked us to forecast, and this is what one of
-    CDC's own surveillance systems is designed to measure.
-    We are not able to measure the number of true cases, deaths are not very indicative of the total strain on the
-    healthcare system, etc.
+    Answer: There are indeed other valuable measures to forecast.  In
+    another project (not crowdsourced) we are forecasting future
+    demand on hospitals.
   </p>
 
 
