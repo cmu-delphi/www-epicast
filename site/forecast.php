03eadadd1313b9abda5008b5b86d47b7bb5d6629
Kathryn M Mazaitis <krivard@cs.cmu.edu>
2020-04-01 16:58:04 -0400

    Dots in forecast curve are now back

--- site/forecast.php
+++ site/forecast.php
@@ -130,23 +130,25 @@ $seasonYears = array_reverse($seasonYears);
 // }
 
 
+ 
 $sources = array(
     "ECDC" => array(
 	"fn" => "getECDCILI",
 	"key" => "ecdc",
-	"members" => array(
+	"members" => array( 
 	    //"Italy" => 8001,
 	    //"Spain" => 8002,
-	    "France" => 8003,
-        "Netherlands" => 8004,
-        "Ireland" => 8005,
-        "UK - Scotland" => 8006,
-        "Belgium" => 8007,
+	    //"France" => 8003,
+        //"Netherlands" => 8004,
+        //"Ireland" => 8005,
+        //"UK - Scotland" => 8006,
+        //"Belgium" => 8007,
     )));
 $sourceIDs = array();
 foreach($sources["ECDC"]["members"] as $country => $cid) {
     $sourceIDs[$cid] = $country.", ECDC";
-}
+} 
+
 
 // $lastOffset = $seasonOffsets[end]
 // for $src,$map in $sources {
@@ -156,6 +158,7 @@ foreach($sources["ECDC"]["members"] as $country => $cid) {
 //      push $rid onto the end of $seasonYears
 //   }
 // }
+
 
 $lastOffset_i = count($seasonOffsets);
 $currentYear = $seasonYears[$lastOffset_i-1];
@@ -260,7 +263,7 @@ foreach($output['regions'] as $r) {
          <div style="clear: both;"></div>
       </div>
 
-       <div class="box_status_line"><div style="margin:20px 180px 10px 180px; padding:5px 40px; background:white; font-weight:normal; font-size:12px; text-align:left"><p><b>Additional data for the 2019-2020 COVID-19 pandemic</b></p><p>The European Centre for Disease Control (ECDC) publishes ILI data for its member nations. COVID-19 reached Italy and several other EU nations ahead of the USA, and the ECDC ILI data for those countries may be useful to your forecasts. This is a rapidly changing situation and not all ECDC reporting countries seem to agree on whether COVID-19 encounters count as ILI activity. We have excluded counties whose ECDC reporting is suspiciously similar to their 2018-2019 season, which unfortunately includes Italy and Spain. Germany is also ahead of us, but does not report ILI data. The ECDC ILI units are not a percent of visits, so <b>while the shape of the curves is accurate, the y-values have been scaled to fit in the plot</b>. For more information on the ECDC data, see the methods section of <a href="https://www.ecdc.europa.eu/sites/default/files/documents/AER_for_2015-influenza-seasonal_0.pdf">the 2015 surveillance report on seasonal Influenza</a>.</p></div></div>
+<!--       <div class="box_status_line"><div style="margin:20px 180px 10px 180px; padding:5px 40px; background:white; font-weight:normal; font-size:12px; text-align:left"><p><b>Additional data for the 2019-2020 COVID-19 pandemic</b></p><p>The European Centre for Disease Control (ECDC) publishes ILI data for its member nations. COVID-19 reached Italy and several other EU nations ahead of the USA, and the ECDC ILI data for those countries may be useful to your forecasts. This is a rapidly changing situation and not all ECDC reporting countries seem to agree on whether COVID-19 encounters count as ILI activity. We have excluded counties whose ECDC reporting is suspiciously similar to their 2018-2019 season, which unfortunately includes Italy and Spain. Germany is also ahead of us, but does not report ILI data. The ECDC ILI units are not a percent of visits, so <b>while the shape of the curves is accurate, the y-values have been scaled to fit in the plot</b>. For more information on the ECDC data, see the methods section of <a href="https://www.ecdc.europa.eu/sites/default/files/documents/AER_for_2015-influenza-seasonal_0.pdf">the 2015 surveillance report on seasonal Influenza</a>.</p></div></div> -->
 
    </div>
    <div id="box_side_bar">
@@ -492,8 +495,9 @@ foreach($output['regions'] as $r) {
       g.lineWidth = style.size * uiScale;
       g.setLineDash(style.dash);
       g.beginPath();
-      var first = true;
+       var first = true;
        var epiweek = addEpiweeks(xRange[0], epiweekOffset);
+       var xs = []; 
       for(var i = start; i < end; i++) {
          if(curve[i] >= 0) {
             var x = getX(epiweek);
@@ -505,11 +509,13 @@ foreach($output['regions'] as $r) {
                g.lineTo(x, y);
             }
          }
+	  xs[i] = epiweek;
          epiweek = addEpiweeks(epiweek, 1);
       }
       g.stroke();
       g.setLineDash([]);
-       if(DRAW_POINTS) drawPoints(curve, start, end, epiweekOffset, style, g);
+       if(DRAW_POINTS)
+	   drawPoints(xs, curve, start, end, style, g);
    }
     function drawCurveXY(xs, ys, start, end, style) {
       var g = getGraphics();
